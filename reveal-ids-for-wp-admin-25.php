56b8a56206987adcbca0e19175def6372b6cfe7b
Oliver Schlöbe <github@schloebe.de>
2021-08-07 14:44:59 +0200

    WordPress 5.8 compatibility

--- reveal-ids-for-wp-admin-25.php
+++ reveal-ids-for-wp-admin-25.php
@@ -9,7 +9,7 @@ Author URI: https://www.schloebe.de/
 Text Domain: reveal-ids-for-wp-admin-25
 Domain Path: /languages
 
-Copyright 2008-2019 Oliver Schlöbe (email : scripts@schloebe.de)
+Copyright 2008-2021 Oliver Schlöbe (email : scripts@schloebe.de)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
